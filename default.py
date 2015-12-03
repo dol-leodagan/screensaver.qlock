e3bdb8fe044fccb6596f4275c0210ac023794aab
phil65 <philipptemminghoff@gmx.de>
2015-12-04 00:02:20 +0100

    cleanup

--- default.py
+++ default.py
@@ -1,14 +1,14 @@
-#Qlock screensaver add-on by phil65
-#credits to donabi & amet
+# Qlock screensaver add-on by phil65
+# credits to donabi & amet
 
 import os
 import sys
 import xbmcaddon
 
-__addon__    = xbmcaddon.Addon()
-__addonid__  = __addon__.getAddonInfo('id')
-__cwd__      = __addon__.getAddonInfo('path').decode("utf-8")
-__resource__ = xbmc.translatePath( os.path.join( __cwd__, 'resources', 'lib' ).encode("utf-8") ).decode("utf-8")
+__addon__ = xbmcaddon.Addon()
+__addonid__ = __addon__.getAddonInfo('id')
+__cwd__ = __addon__.getAddonInfo('path').decode("utf-8")
+__resource__ = xbmc.translatePath(os.path.join(__cwd__, 'resources', 'lib').encode("utf-8")).decode("utf-8")
 
 sys.path.append(__resource__)
 
@@ -16,5 +16,3 @@ if __name__ == '__main__':
     import gui
     screensaver_gui = gui.Screensaver('script-python-qlock.xml', __cwd__, 'default')
     screensaver_gui.doModal()
-    del screensaver_gui
-    sys.modules.clear()
